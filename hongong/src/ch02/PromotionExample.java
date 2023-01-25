977e8f113a19e4b1f685fd931a7d319a5bf8abb4
imtaeyi <mintchodan@naver.com>
2023-01-25 23:42:10 +0900

    변수

---
+++ hongong/src/ch02/PromotionExample.java
@@ -0,0 +1,15 @@
+package ch02;
+
+public class PromotionExample {
+	public static void main(String[] args) {
+		// 자동 타입 변환
+		byte byteValue = 10;
+		int intValue = byteValue;
+		System.out.println("intValue: "+intValue);
+		
+		char charValue = '가';
+		intValue = charValue;
+		System.out.println("가의 유니코드:"+intValue);
+	}
+
+}
