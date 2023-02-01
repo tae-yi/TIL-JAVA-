2c0968d7eb00add901ff7e562f8201b30b4ec8d2
imtaeyi <mintchodan@naver.com>
2023-02-02 00:43:21 +0900

    제어문

---
+++ hongong/src/ch04/ForSumFromTo100Example1.java
@@ -0,0 +1,14 @@
+package ch04;
+
+public class ForSumFromTo100Example1 {
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		int i = 0;
+		for(i = 1; i <= 100; i++) {
+			sum += i;
+		}
+		
+		System.out.println("1~" + (i-1) + " 합 : " + sum);
+	}
+}
---
+++ hongong/src/ch04/whilePrintFrom1To10Example.java
@@ -0,0 +1,12 @@
+package ch04;
+
+public class whilePrintFrom1To10Example {
+	public static void main(String[] args) {
+	int k = 1;
+	while ( k <= 10) {
+		System.out.println(k);
+		k++;
+		}
+	}
+
+}
