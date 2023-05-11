5202927951b3b539ac2b3b73ab4ed8791c24da8f
imtaeyi <mintchodan@naver.com>
2023-05-12 00:24:01 +0900

    인터페이스

--- lecture/src/chap_08/camera/FactoryCam.java
+++ lecture/src/chap_08/camera/FactoryCam.java
@@ -1,9 +1,35 @@
 package chap_08.camera;
 
-public class FactoryCam extends Camera {
+import chap_08.detector.Detectable;
+import chap_08.reporter.Reportable;
+
+public class FactoryCam extends Camera implements Detectable, Reportable {
+    // Detectable, Reportable 인터페이스의 기능을 사용할 수 있게됨
+
+    private Detectable detector; // 객체
+    private Reportable reporter; // 객체
+
+    public void setDetector(Detectable detector) {
+        this.detector = detector;
+    }
+
+    public void setReporter(Reportable reporter) {
+        this.reporter = reporter;
+    }
 
     @Override
     public void showMainFeature() { // Camera클래스를 상속받기때문에 Camera클래스의 추상메소드를 구현해야 객체를 만들 수 있음
         System.out.println("화재 감지"); 
     }
+
+    @Override
+    public void detect() {
+        this.detector.detect();
+    }
+
+    @Override
+    public void report() {
+        this.reporter.report();
+    }
+
 }
--- lecture/src/chap_08/camera/SpeedCam.java
+++ lecture/src/chap_08/camera/SpeedCam.java
@@ -1,8 +1,30 @@
 package chap_08.camera;
 
+import chap_08.detector.Detectable;
+import chap_08.reporter.Reportable;
+
 public class SpeedCam extends Camera{
+    private Detectable detector;
+    private Reportable reporter;
+
+    public void setDetector(Detectable detector) {
+        this.detector = detector;
+    }
+
+    public void setReporter(Reportable reportable) {
+        this.reporter = reportable;
+    }
+
     @Override
     public void showMainFeature() { // Camera클래스를 상속받기때문에 Camera클래스의 추상메소드를 구현해야 객체를 만들 수 있음
         System.out.println("속도 측정, 번호 인식");
+    }
+
+    public void detect() {
+        this.detector.detect();
+    }
+
+    public void report() {
+        this.reporter.report();
     }
 }
